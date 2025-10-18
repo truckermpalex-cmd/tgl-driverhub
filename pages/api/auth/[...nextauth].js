972fda429df46566d89f04a383398f81981a361f
truckermpalex-cmd <truckermpalex@gmail.com>
2025-10-18 11:46:28 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import DiscordProvider from 'next-auth/providers/discord'
 import EmailProvider from 'next-auth/providers/email'
-import clientPromise from '../../../lib/mongodb'
+import clientPromise from '../../../../lib/mongodb';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 
 export default NextAuth({
